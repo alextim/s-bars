429a661e7d502b6ceb8b6187b2019d007c1ecb49
Alex Tim <buildmnt@gmail.com>
2021-05-05 10:56:10 +0300

    field-utils

--- functions/utils/field-utils.js
+++ functions/utils/field-utils.js
@@ -7,21 +7,26 @@ const validateField = (x, fieldInfo) => {
   if (!x) {
     return !required;
   }
+
   const val = x.trim();
   const n = val.length;
   if (n < minLength || n > maxLength) {
     return false;
   }
+
   const sanitized = sanitizeHtml(val);
   if (sanitized.length < minLength) {
     return false;
   }
+
   if (pattern && !new RegExp(pattern).exec(sanitized)) {
     return false;
   }
+
   if (validator && !validator(sanitized)) {
     return false;
   }
   return true;
 };
+
 module.exports = { sanitizeField, validateField };
